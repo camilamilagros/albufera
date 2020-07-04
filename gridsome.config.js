b9bdcc338b1b66d953f8a0742d7774446cbf2177
Camila <camila.chavez@pucp.pe>
2020-07-03 20:00:19 -0500

    Tailwind fix

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,20 +4,11 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-const tailwind = require('tailwindcss');
-
-const postcssPlugins = [
-  tailwind()
-];
-
 module.exports = {
   siteName: 'Camila | To do',
-  plugins: [],
-  css: {
-    loaderOptions: {
-      postcss: {
-        plugins: postcssPlugins,
-      }
-    }
-  }
+  plugins: [
+    {
+      use: "gridsome-plugin-tailwindcss",
+    },
+  ],
 }
--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue';
-import "./tailwind.css";
-
 
 export default function (Vue, { router, head, isClient }) {
   head.htmlAttrs = { class: 'h-full' }
